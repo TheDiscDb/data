73030c9229cc614139dfaedc7b81aff4faf0d4bd
Luke Foust <luke@foust.com>
2025-08-22 22:04:42 -0700

    Add the client project which I guess I forgot before

---
+++ tools/ImportBuddy/source/ImportBuddy/TheDiscDb.Client/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddTheDiscDbClient(this IServiceCollection services, Uri baseAddress)
+    {
+        services
+        .AddTheDiscDbClient()
+        .ConfigureHttpClient(client => client.BaseAddress = baseAddress);
+
+        return services;
+    }
+}
